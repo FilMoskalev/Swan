424982f060bd5f1cf66560880b3fb48da76ec889
PLARIUM\p.moskalev <p.moskalev>
2017-03-22 17:45:17 +0200

    add methods

--- Swan/Controllers/AccountCotroller.cs
+++ Swan/Controllers/AccountCotroller.cs
@@ -1,4 +1,6 @@
-﻿using System.Linq;
+﻿using System.Data.Entity;
+using System.Linq;
+using System.Net;
 using System.Web.Mvc;
 using System.Web.Security;
 using Data.DataModel;
@@ -78,7 +80,65 @@ namespace Swan.Controllers
         [Authorize(Roles = "admin")]
         public ActionResult ShowAllUsers()
         {
-          return View(_db.Users.Where(c=>true));
+          return View(_db.Users.Where(c=> true).ToList());
+        }
+
+
+        [Authorize(Roles = "admin")]
+        public ActionResult Edit(int? id)
+        {
+          if (id == null)
+          {
+            return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+          }
+          User user = _db.Users.Find(id);
+          if (user == null)
+          {
+            return HttpNotFound();
+          }
+          ViewBag.RoleId = new SelectList(_db.Roles, "Id", "Name", user.RoleId);
+          return View(user);
+        }
+
+        [Authorize(Roles = "admin")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Email,Password,CreationDate,RoleId")] User user)
+        {
+          if (ModelState.IsValid)
+          {
+            _db.Entry(user).State = EntityState.Modified;
+            _db.SaveChanges();
+            return RedirectToAction("ShowAllUsers", "Account");
+          }
+          ViewBag.RoleId = new SelectList(_db.Roles, "Id", "Name", user.RoleId);
+          return View(user);
+        }
+
+        [Authorize(Roles = "admin")]
+        public ActionResult Delete(int? id)
+        {
+          if (id == null)
+          {
+            return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+          }
+          User user = _db.Users.Find(id);
+          if (user == null)
+          {
+            return HttpNotFound();
+          }
+          return View(user);
+        }
+
+        [Authorize(Roles = "admin")]
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+          User user = _db.Users.Find(id);
+          _db.Users.Remove(user);
+          _db.SaveChanges();
+          return RedirectToAction("ShowAllUsers", "Account");
         }
 
         protected override void Dispose(bool disposing)
